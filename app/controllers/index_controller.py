9613b26b1e6402a7938a33fbfdac37bc0aed81cd
Kartha97 <karthaveera.sgs@gmail.com>
2022-02-07 16:10:50 -0500

    Changing name in indexController

--- app/controllers/index_controller.py
+++ app/controllers/index_controller.py
@@ -6,5 +6,5 @@ from  . controller import ControllerBase
 class IndexController(ControllerBase):
     @staticmethod
     def get():
-        name = "Keith Williams 3"
+        name = "Kartha"
         return render_template('index.html', name=name)
